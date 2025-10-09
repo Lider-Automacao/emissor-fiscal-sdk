59b967b3f6f4f242f8984abede5adc75a789968b
Raiane Campos <raianejanu@hotmail.com>
2025-10-09 11:41:56 -0300

    feat: atualiza a versão para 1.0.14 e corrige tipo do campo 'host' no schema de configuração de email

--- src/models/configuracoes.ts
+++ src/models/configuracoes.ts
@@ -7,7 +7,7 @@ export const ConfiguracaoCertificadoSchema = z.object({
 })
 
 export const ConfiguracaoEmailSchema = z.object({
-  host: z.url().trim().nonempty(),
+  host: z.string().trim().nonempty(),
   password: z.string().trim().nonempty(),
   port: z.coerce.string().trim().nonempty().regex(/^\d+$/, 'A porta deve ser um número inteiro.'),
   userName: z.string().trim().nonempty(),
